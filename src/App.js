607c59edddada2e242dc9873a4e81323de0d7546
clvalada <clvalada@gmail.com>
2024-01-17 11:07:51 -0500

    updating fille structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Resume from './Resume';
+import Navbar from './Navbar';
 
-function App() {
+// Define App component
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Navbar />
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="Hats" element={<Hats />} />
+          <Route path="Shirts" element={<Shirts />} />
+          <Route path="Other" element={<Contact />} />
+          <Route path="Blog" element={<Blog />} />
+        </Routes>
+      </div>
+   </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+//Navbar.js
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (  
+      <nav className="navbar">
+        <h1>Chris Valada</h1>
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/Hats">Hats</Link>
+          <Link to="/Shirts">Shirts</Link>
+          <Link to="/Other">Other</Link>
+          <Link to="/Blog">Blog</Link>
+          <Link to="/About">About Us</Link>
+        </div> 
+      </nav>
+    );
+  };
+
+
+export default Navbar;
