e87fe6faf2e2922a95d9c95ac544521cd25a692d
clvalada <clvalada@gmail.com>
2024-01-23 16:36:21 -0500

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,12 @@ const Navbar = () => {
       <nav className="navbar" >
           <h1><a href="/" className="navbar-logo">Vintage Drip</a></h1>
         <div className="Links">
+            <Link to="/">Home</Link>
             <Link to="/Hats">Hats</Link>
             <Link to="/Shirts">Shirts</Link>
             <Link to="/Other">Other</Link>
             {/*<Link to="/About">About Us</Link>*/}
-            <Link to="/Blog">Blog</Link>
+            {/*<Link to="/Blog">Blog</Link>*/}
         </div> 
       </nav>
     );
