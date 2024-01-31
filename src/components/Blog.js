9e38c2fc26f3caef434ae9d0bef0865adca4b944
Chris Valada <145150623+clvalada@users.noreply.github.com>
2024-01-31 14:26:13 -0500

    revising blog component

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,14 +1,31 @@
 //Blog.js
 
 import React from "react";
-import Posts from "./Posts";
-
+import BlogPost from "./BlogPost"; // Assuming you have a BlogPost component
 
 const Blog = () => {
+  const blogPosts = [
+    {
+      id: 1,
+      title: "First Blog Post",
+      content: "This is the content of the first blog post.",
+    },
+    {
+      id: 2,
+      title: "Second Blog Post",
+      content: "This is the content of the second blog post.",
+    },
+    // Add more blog posts as needed
+  ];
+
   return (
     <div>
-        <h1>Stay tuned for the Vintage Drip fasion blog.</h1>
-        <Posts Blog={Blog} />
+      <h1>Blog</h1>
+      <div>
+        {blogPosts.map((post) => (
+          <BlogPost key={post.id} post={post} />
+        ))}
+      </div>
     </div>
   );
 };
